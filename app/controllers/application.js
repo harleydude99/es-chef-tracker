23a250fdc81c877738fcf04c08353be87763045b
Harley Dude <harleydude99@gmail.com>
2017-08-31 14:15:24 -0700

    some ember computed fields

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   moniker: 'chef',
   newChef: null,
+  chefLength: Ember.computed.alias('model.length'),
+  availableChefs: Ember.computed.filterBy('model', 'isCookingToday', true),
   actions:{
     toggleEnterExit(cook){
       Ember.set(cook, 'isCookingToday', !Ember.get(cook, 'isCookingToday'));
@@ -14,6 +16,9 @@ export default Ember.Controller.extend({
         name: this.get('newChef')
       }).save()
       this.set('newChef', '')
+    },
+    fireChef(chef){
+      chef.destroyRecord()
     }
   }
 });
