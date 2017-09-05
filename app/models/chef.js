3ef9a47d0f7401a4c5e742fbfbdbff4f77833bfb
Harley Dude <harleydude99@gmail.com>
2017-09-05 14:26:57 -0700

    routes, nested route, detail id linking

--- app/models/chef.js
+++ app/models/chef.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   isCookingToday: DS.attr('boolean'),
-  numOfStudents: DS.attr('number', {defaultValue: 0})
+  numOfStudents: DS.attr('number', {defaultValue: 0}),
+  description: DS.attr('string')
 });
