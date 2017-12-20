3f2b6b724e21000bb60702f95ae8462528f0ae10
Harley Dude <harleydude99@gmail.com>
2017-12-20 12:16:48 -0700

    end of dynamic checkbox challenge

--- app/controllers/chefs/chef/edit.js
+++ app/controllers/chefs/chef/edit.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(){
-      this.get('model').save();
+      let cook = this.get('model');
+      console.log(cook.data);
+      if(!cook.data.hasCulinaryDegree){
+        Ember.set(cook, 'culinarySchoolAttended', '');
+      }
+      //Ember.set(cook, 'culinarySchoolAttended', !Ember.get(cook, 'isCookingToday'));
+      //this.get('model').save();
+      cook.save();
       this.transitionToRoute('chefs.chef', this.get('model.id'));
     }
   }
--- app/models/chef.js
+++ app/models/chef.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   isCookingToday: DS.attr('boolean'),
+  hasCulinaryDegree: DS.attr('boolean'),
+  culinarySchoolAttended: DS.attr('string'),
   numOfStudents: DS.attr('number', {defaultValue: 0}),
   description: DS.attr('string')
 });
