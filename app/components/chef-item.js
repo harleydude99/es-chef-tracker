c4300b3aa72bfe056427b65c927f808fdb32e2e3
Harley Dude <harleydude99@gmail.com>
2017-12-26 08:08:21 -0700

    refactor to use chef component

---
+++ app/components/chef-item.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    toggleEnterExit(cook){
+      Ember.set(cook, 'isCookingToday', !Ember.get(cook, 'isCookingToday'));
+      //could also do
+      //cook.set('isCookingToday', !cook.get('isCookingToday'));
+      cook.save();
+    },
+    modifyStudentCount(cook, operation){
+      let currentVal = Ember.get(cook, 'numOfStudents');
+      let newVal = 0
+      if(operation === 'add'){
+        newVal = currentVal+1;
+      }else{
+        if(currentVal > 0){
+          newVal = currentVal-1;
+        }
+      }
+      Ember.set(cook, 'numOfStudents', newVal);
+      cook.save();
+    }
+  }
+});
--- app/controllers/chefs.js
+++ app/controllers/chefs.js
@@ -19,10 +19,7 @@ export default Ember.Controller.extend({
   }),
   */
   actions:{
-    toggleEnterExit(cook){
-      Ember.set(cook, 'isCookingToday', !Ember.get(cook, 'isCookingToday'));
-      cook.save();
-    },
+
     saveNewChef(){
       this.store.createRecord('chef', {
         isAvailable: false,
@@ -40,19 +37,7 @@ export default Ember.Controller.extend({
       //Ember.set(cook, 'numOfStudents', Ember.get(cook, 'numOfStudents')+1);
       Ember.incrementProperty('numOfStudents');
       cook.save();
-    },
-    modifyStudentCount(cook, operation){
-      let currentVal = Ember.get(cook, 'numOfStudents');
-      let newVal = 0
-      if(operation === 'add'){
-        newVal = currentVal+1;
-      }else{
-        if(currentVal > 0){
-          newVal = currentVal-1;
-        }
-      }
-      Ember.set(cook, 'numOfStudents', newVal);
-      cook.save();
     }
+
   }
 });
