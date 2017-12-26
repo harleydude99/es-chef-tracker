c679ebcd608d147854087f1de6a5bbf6fd17985d
Harley Dude <harleydude99@gmail.com>
2017-12-26 09:50:56 -0700

    positionalParams for components

--- app/components/icon-image.js
+++ app/components/icon-image.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+}).reopenClass({
+  positionalParams: ['src']  //leaving left a named param
+  //positionalParams: ['src', 'left']
 });
