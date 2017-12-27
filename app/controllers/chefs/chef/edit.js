971e3e5a71d092a619ab6068560c78945bb71d36
Harley Dude <harleydude99@gmail.com>
2017-12-27 14:25:57 -0700

    chef assignment exercise, emberx-select

--- app/controllers/chefs/chef/edit.js
+++ app/controllers/chefs/chef/edit.js
@@ -3,14 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(){
-      let cook = this.get('model');
+      let cook = this.get('model.chef');
       if(!cook.data.hasCulinaryDegree){
         Ember.set(cook, 'culinarySchoolAttended', '');
       }
       //Ember.set(cook, 'culinarySchoolAttended', !Ember.get(cook, 'isCookingToday'));
       //this.get('model').save();
       cook.save();
-      this.transitionToRoute('chefs.chef', this.get('model.id'));
-    }
+      this.transitionToRoute('chefs.chef', this.get('model.chef.id'));
+    },
+    selectRestaurant(selection, component){
+  		//selection is the NEW selection
+  		//old is still in the chef
+  		let chef = this.get('model.chef');
+  		chef.get('restaurant').then((restaurant)=>{
+  			chef.set('restaurant', selection);
+  			selection.save();//save new
+  			restaurant.save();//save old as well
+  		});
+	  }
   }
 });
